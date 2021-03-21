cf546f819dd8335621abfc1a268436a53643914f
Peter Prevos <peter@prevos.net>
2021-03-21 17:57:40 +1100

    Updated biorythm

--- biorhythm.R
+++ biorhythm.R
@@ -1,7 +1,8 @@
 # Biorhythm
 
 library(ggplot2)
-library(reshape2)
+library(dplyr)
+library(tidyr)
 
 biorhythm <- function(dob, target = Sys.Date()) {
     dob <- as.Date(dob)
@@ -11,21 +12,19 @@ biorhythm <- function(dob, target = Sys.Date()) {
     period <- data.frame(Date = seq.Date(from = target - 14, by = 1, length.out = 29),
                          Physical = sin (2 * pi * days / 23) * 100, 
                          Emotional = sin (2 * pi * days / 28) * 100, 
-                         Intellectual = sin (2 * pi * days / 33) * 100)
-    period <- melt(period, id.vars = "Date", variable.name = "Biorhythm", value.name = "Percentage")
+                         Intellectual = sin (2 * pi * days / 33) * 100) %>%
+              pivot_longer(-Date, names_to = "Biorhythm", values_to = "Percentage")
     ggplot(period, aes(x = Date, y = Percentage, col = Biorhythm)) + geom_line() +  
         ggtitle(paste("DoB:", format(dob, "%d %B %Y"))) + 
         geom_vline(xintercept = as.numeric(target))
 }
 
-biorhythm("1969-09-12", "2017-03-30")
+biorhythm("1969-09-12")
 ggsave("biorhythm.png")
 
-
 dob <- as.Date("1969-09-12")
-target <- dob + 23*28*33
+target <- dob + 23 *28 * 33
 biorhythm(dob, target)
-
 
 bio.max <- 0
 day.max <- NA
