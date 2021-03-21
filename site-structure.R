a6fa63c98379a50a40534522a7a46a4cbbce661e
Peter Prevos <peter@prevos.net>
2021-03-21 18:00:30 +1100

    Global update

--- site-structure.R
+++ site-structure.R
@@ -14,24 +14,20 @@ source("dbconnect.R")
 ##                          host = "URL")
 
 posts.query <- "SELECT p.post_name, p.post_content, t.name
-FROM wp_posts p
-JOIN wp_term_relationships tr ON (tr.object_id = p.ID)
-JOIN wp_term_taxonomy tt ON (tt.term_taxonomy_id = tr.term_taxonomy_id )
-JOIN wp_terms t ON (t.term_id = tt.term_id)
-WHERE
-    p.post_type='post'
-AND
-    p.post_status='publish'
-AND
-    tt.taxonomy = 'category'"
+                FROM wp_posts p
+                JOIN wp_term_relationships tr ON (tr.object_id = p.ID)
+                JOIN wp_term_taxonomy tt ON (tt.term_taxonomy_id = tr.term_taxonomy_id )
+                JOIN wp_terms t ON (t.term_id = tt.term_id)
+                WHERE
+                 p.post_type='post'
+                 AND
+                 p.post_status='publish'
+                 AND
+                 tt.taxonomy = 'category'"
 
 pages.query <- "SELECT post_name, post_content
-FROM
-    wp_posts
-WHERE
-    post_type='page'
-AND
-post_status='publish'"
+                FROM wp_posts
+                WHERE post_type='page' AND post_status='publish'"
 
 posts.data <- dbSendQuery(lucidmanager, posts.query)
 posts <- fetch(posts.data, n=-1)
@@ -45,18 +41,22 @@ pages$name = "Page"
 content <- rbind(posts, pages)
 as_tibble(content)
 
+## Extract internal links
 links <- content %>%
-  unnest_tokens(word, post_content,
-                             token = stringr::str_split, pattern = " ") %>%
+    unnest_tokens(word, post_content,
+                  token = stringr::str_split, pattern = " ") %>%
     filter(grepl(".+lucidmanager.org/", word)) %>%
     mutate(link = gsub(".+lucidmanager.org/", "", word),
            link = gsub("/.+", "", link)) %>%
     filter(link != "tag" & link !=  "wp-content" & link != "author") %>%
     select(category = name, post_name, link)
+
 tibble(links)
 
 solitary <- c(posts$post_name, pages$post_name)[!(c(posts$post_name, pages$post_name) %in%
                                                   unique(c(links$post_name, links$link)))]
+
+## Create network
 network <- select(links, post_name, link) %>%
   graph_from_data_frame(directed = TRUE) + 
   vertices(solitary)
